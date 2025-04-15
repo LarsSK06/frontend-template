f3af06cd904a7dfe54eade1601a6ca5c0fb21bf0
Lars Synslien Kvihaugen <lars@kvihaugen.no>
2025-04-15 20:15:25 +0200

    [main] Removed bad spawner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 
-import Spawner from "@/components/Spawner";
-
 import { Button } from "@mantine/core";
 import { t } from "i18next";
 
 const Page = () => {
     return (
-        <Spawner pre={{ opacity: 0, transform: "translateY(-10px)" }} style={{ display: "block", transition: "1s ease" }}>
-            <Button>
-                {t("general.HelloWorld")}
-            </Button>
-        </Spawner>
+        <Button>
+            {t("general.HelloWorld")}
+        </Button>
     );
 };
 
